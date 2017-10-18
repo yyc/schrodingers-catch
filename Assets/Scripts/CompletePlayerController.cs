5330f55c54bccd256a80f9c3ddeb030310109d58
Yuchuan <yyc478@gmail.com>
2017-10-19 03:42:04 +0800

    Store coordinates instead of Vectors as position

--- Assets/Scripts/CompletePlayerController.cs
+++ Assets/Scripts/CompletePlayerController.cs
@@ -11,15 +11,24 @@ public class CompletePlayerController : MonoBehaviour {
 	private int count;				//Integer to store the number of pickups collected so far.
 	private Timekeeper timekeeper;
 	private Hashtable hashtable;
-	public Tuple<int, int> gridCoords;
+	public Tuple3<int, int, int> storedPosition;
 	public float cooldown = 0.1f;
 	private float lastPressTime = 0f;
+
+	Quaternion[] directions = new Quaternion[4] { // [left, right][up, down]
+		Quaternion.identity, Quaternion.identity,
+		Quaternion.identity, Quaternion.identity
+	};
 
 	// Use this for initialization
 	void Start()
 	{
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
+		directions [0] = Quaternion.AngleAxis (0, new Vector3 (0, 0, 1));
+		directions [1] = Quaternion.AngleAxis (90, new Vector3 (0, 0, 1));
+		directions [2] = Quaternion.AngleAxis (180, new Vector3 (0, 0, 1));
+		directions [3] = Quaternion.AngleAxis (270, new Vector3 (0, 0, 1));
 	}
 
 	void Awake(){
@@ -45,28 +54,30 @@ public class CompletePlayerController : MonoBehaviour {
 
 				if (moveHorizontal != 0) {
 					lastPressTime = timekeeper.getTime ();
-					gridCoords = new Tuple<int, int> (gridCoords.first, gridCoords.second + moveHorizontal);
+					// direction = 1 if right (moveHorizontal  =1)
+					// direction = 3 if left (moveHorizontal = -1)
+					storedPosition = new Tuple3<int, int, int> (storedPosition.first, storedPosition.second + moveHorizontal, 1 - moveHorizontal);
+
 				} else if (moveVertical != 0) {
 					lastPressTime = timekeeper.getTime ();
-					gridCoords = new Tuple<int, int> (gridCoords.first + moveVertical, gridCoords.second);
+					// direction = 2 if up (moveVertical  =1)
+					// direction = 4 if down (moveVertical = -1)
+					storedPosition = new Tuple3<int, int, int> (storedPosition.first + moveVertical, storedPosition.second, 2 - moveVertical);
 				}
-
 			}
 
-
-
-			hashtable.Add (timekeeper.getTime (), gridCoords);
-
+			hashtable.Add (timekeeper.getTime (), storedPosition);
 
 			//move to appropriate coordinate
 
 
 		} else {
 			if (hashtable.ContainsKey (timekeeper.getTime ())) {
-				gridCoords = (Tuple<int, int>) hashtable [timekeeper.getTime ()];
+				storedPosition = (Tuple3<int, int, int>) hashtable [timekeeper.getTime ()];
 			}
 		}
-		transform.position = MapGenerator.PositionFor (gridCoords, -1);
+		transform.position = MapGenerator.PositionFor (storedPosition, -1);
+		transform.rotation = directions [storedPosition.third];
 	}
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -32,8 +32,7 @@ public class MapGenerator : MonoBehaviour {
 		tileHeight = tilePrefab.transform.localScale.y;
 		tileWidth = tileHeight; // make it square for now
 
-		Vector3 charStart = new Vector3(0,0,0);
-		Tuple<int, int> charCoords = new Tuple<int, int> (0, 0);
+		Tuple3<int, int, int> charCoords = new Tuple3<int, int, int> (0, 0,0);
 
 		for (int i = 0; i < numRows; i++) {
 			for (int j = 0; j < numCols; j++) {
@@ -42,15 +41,20 @@ public class MapGenerator : MonoBehaviour {
 				GameObject newTile = Instantiate (tilePrefab, position, Quaternion.identity);
 				newTile.GetComponent<SpriteRenderer> ().sprite = sprites [map [i, j]];
 				if(map[i, j] == 2){ // Store portal location
-					charCoords = new Tuple<int, int> (i, j);
+					charCoords = new Tuple3<int, int, int> (i, j, 0);
 				}
 			}
 		}
 
 		character.transform.position = PositionFor(charCoords);
-		character.GetComponent<CompletePlayerController> ().gridCoords = charCoords;
+		character.GetComponent<CompletePlayerController> ().storedPosition = charCoords;
 	
 	}
+
+	public static Vector3 PositionFor(Tuple3<int, int, int> tuple, float z = 0) {
+		return PositionFor (tuple.first, tuple.second, z);
+	}
+
 
 	public static Vector3 PositionFor(Tuple<int, int> tuple, float z = 0) {
 		return PositionFor (tuple.first, tuple.second, z);
