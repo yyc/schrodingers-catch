83f925f69371a97fda32de5f2cd091fc4678bb4c
Yuchuan <yyc478@gmail.com>
2017-10-23 01:20:33 +0800

    add enemy components and systems

---
+++ Assets/Scripts/Systems/EnemyControllerSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControllerSystem : MonoBehaviour {
+  // Use this for initialization
+  public float speed = 3;
+  void Start() {}
+
+  // Update is called once per frame
+  // No Update, all paths are generated at runtime
+  //  void Update() {}
+}
---
+++ Assets/Scripts/Systems/EnemySpawnSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unitilities.Tuples;
+
+public class EnemySpawnSystem : MonoBehaviour {
+  int currentEnemies = 0;
+  public GameObject enemyPrefab;
+
+  // Use this for initialization
+  void Start() {}
+
+  // Update is called once per frame
+  void Update() {
+    if (currentEnemies == 0) {
+      Spawn(new int[1] {
+        2
+      });
+    }
+  }
+
+  void Spawn(int[] enemies) {
+    for (int i = 0; i < enemies.Length; i++) {
+      for (int j = 0; j < enemies[i]; j++) {
+        GameObject newEnemy = Instantiate(enemyPrefab,
+                                          Vector3.zero,
+                                          Quaternion.identity);
+
+        MemoryComponent memComponent = newEnemy.GetComponent<MemoryComponent>();
+        memComponent.position = new Tuple3I(i, j, 0);
+        currentEnemies++;
+      }
+    }
+  }
+}
