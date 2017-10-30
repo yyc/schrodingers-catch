6b46612b291258c940a73bb0b53a29571cfaf4cf
Yuchuan <yyc478@gmail.com>
2017-10-30 11:44:03 +0800

    Add MenuEnemySystem

---
+++ Assets/Scripts/Systems/MenuEnemySystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEnemySystem : MonoBehaviour {
+  public string transitionScene = "";
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    // Check the provided Collider2D parameter other to see if it is tagged
+    // "PickUp", if it is...
+    Debug.Log("entered");
+    Debug.Log(other.gameObject);
+
+    if (other.gameObject.CompareTag("Player")) {
+      Debug.Log("tagged");
+      SceneManager.LoadScene(transitionScene);
+    }
+  }
+}
