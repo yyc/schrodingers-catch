11b830dc5351de0ecafce862003536e3d213ff93
Yuchuan <yyc478@gmail.com>
2017-11-06 15:59:07 +0800

    Add scene button component

---
+++ Assets/Scripts/Components/SceneButtonComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneButtonComponent : MonoBehaviour {
+  public string sceneName;
+
+  // Use this for initialization
+  void Start() {
+    Button btn = GetComponent<Button>();
+
+    btn.onClick.AddListener(TransitionOnClick);
+  }
+
+  // Update is called once per frame
+  void TransitionOnClick() {
+    SceneManager.LoadScene(sceneName);
+  }
+}
