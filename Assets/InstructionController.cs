96e7d5b6e4b5266e018bd0ae37e3dab119af3d5b
Yuchuan <yyc478@gmail.com>
2017-11-10 06:40:08 +0800

    Get instructions working

---
+++ Assets/InstructionController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionController : MonoBehaviour {
+  public List<GameObject>panels;
+  public Button nextButton;
+  public Button prevButton;
+  public int panelIndex = 0;
+
+  // Use this for initialization
+  void Start()  {
+    UpdatePanels();
+    nextButton.onClick.AddListener(Next);
+    prevButton.onClick.AddListener(Previous);
+  }
+
+  // Update is called once per frame
+  void UpdatePanels() {
+    for (int i = 0; i < panels.Count; i++) {
+      panels[i].SetActive(i == panelIndex);
+    }
+
+    if (panelIndex == 0) {
+      prevButton.gameObject.SetActive(false);
+    } else if (panelIndex == panels.Count - 1) {
+      nextButton.gameObject.SetActive(false);
+    } else {
+      nextButton.gameObject.SetActive(true);
+      prevButton.gameObject.SetActive(true);
+    }
+  }
+
+  void Next() {
+    if (panelIndex == panels.Count - 1) {
+      return;
+    }
+    panelIndex++;
+    UpdatePanels();
+  }
+
+  void Previous() {
+    if (panelIndex == 0) {
+      return;
+    }
+    panelIndex--;
+    UpdatePanels();
+  }
+}
