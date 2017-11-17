3695788fcd00379c52fe86205f8e14a390eab0ee
Yuchuan <yyc478@gmail.com>
2017-11-17 18:12:23 +0800

    add menu loading

--- Assets/Scripts/Systems/MenuEnemySystem.cs
+++ Assets/Scripts/Systems/MenuEnemySystem.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuEnemySystem : MonoBehaviour {
   public string transitionScene = "";
+  public float minimumLoadingTime = 3.0f;
+  public Text loadingText;
 
+  float startTime;
+  Timekeeper timekeeper;
+  AsyncOperation menuLoad;
+  string originalText;
+  bool hasEntered = false;
+
+  void Start() {
+    if(loadingText != null) {
+      originalText = loadingText.text;
+    }
+  }
+  void Update() {
+    if(hasEntered == true) {
+      if((timekeeper.getTime() - startTime) > 0.90 * minimumLoadingTime && menuLoad == null) {
+        Debug.Log("canLoad");
+        menuLoad = SceneManager.LoadSceneAsync(transitionScene);
+      }
+      float progress = (Mathf.Min((timekeeper.getTime() - startTime) / minimumLoadingTime, 0.9f) + 
+            (0.1f * (menuLoad == null ? 0 : menuLoad.progress))) * 100;
+      loadingText.text = "Loading " + Mathf.RoundToInt(progress) + "%";
+    }
+  }
   void OnTriggerEnter2D(Collider2D other)
   {
     // Check the provided Collider2D parameter other to see if it is tagged
     // "PickUp", if it is...
 
     if (other.gameObject.CompareTag("Player")) {
-      SceneManager.LoadScene(transitionScene);
+      timekeeper = Timekeeper.getInstance();
+      startTime = timekeeper.getTime();
+      hasEntered = true;
+    }
+  }
+
+  void OnTriggerExit2D(Collider2D other) {
+    Debug.Log("Exited");
+    if (other.gameObject.CompareTag("Player")) {
+      loadingText.text = originalText;
+      hasEntered = false;
     }
   }
 }
