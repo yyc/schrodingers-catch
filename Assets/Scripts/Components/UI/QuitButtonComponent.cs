2d4ad489c93ff0e876013cd50437783c50cab07a
Yuchuan <yyc478@gmail.com>
2017-11-17 18:29:03 +0800

    Add quit button and panel

---
+++ Assets/Scripts/Components/UI/QuitButtonComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButtonComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(Quit);
+	}
+	
+	// Update is called once per frame
+	void Quit () {
+		Application.Quit();
+	}
+}
